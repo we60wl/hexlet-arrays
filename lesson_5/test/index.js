8805b3cb93acaf956a81ccd70f72ec79b3de577d
we60wl <naxi03@mail.ru>
2025-11-30 23:29:18 +0300

    feat(app): unique

--- lesson_5/test/index.js
+++ lesson_5/test/index.js
@@ -1,4 +1,15 @@
-const names = ['john', 'smith', 'karl']
+const names = ['john', 'smith', 'karl', 'john']
+
+const unique = (coll) => {
+  const init = []
+  return coll.reduce((acc, current) => {
+    if (acc.includes(current)) {
+      return acc
+    }
+    return [...acc, current]
+  }, init)
+}
+console.log(unique(names))
 
 reverse(names)
 console.log(names) // => ['karl', 'smith', 'john']
@@ -9,3 +20,5 @@ console.log(names) // => ['john', 'smith', 'karl']
 function reverse(names) {
   return names.reverse();
 }
+
+
